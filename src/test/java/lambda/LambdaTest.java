8b1c6e96a14d722c738cabf350335f07cde0d76b
jackie <jackie.zheng@ximalaya.com>
2017-09-17 12:49:03 +0800

    update

--- src/test/java/lambda/LambdaTest.java
+++ src/test/java/lambda/LambdaTest.java
@@ -1,6 +1,5 @@
 package lambda;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
@@ -11,6 +10,7 @@ import java.util.function.Consumer;
 public class LambdaTest {
     public static void main(String[] args) {
         loop(Arrays.asList(1,2,3,4,5), (Integer i)->System.out.println("哈哈"));
+        System.out.println(-1 % 2 == 1 ? "奇数" : "偶数");
     }
 
     private static<T> void loop(List<T> list1, Consumer<T> consumer) {
